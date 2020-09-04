3812620d489278b9b259eb2fc993aa43d3f9d329
thorntondc1 <thorntondc1@gmail.com>
2020-09-04 14:21:07 -0500

    Combined tool and gui. Made textbox wrap

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import wx
-
+import re
 
 class TicketCountFrame(wx.Frame):
     def __init__(self):
@@ -12,13 +12,17 @@ class TicketCountFrame(wx.Frame):
         font.PointSize = 12
         font = font.Bold()
         self.text.SetFont(font)
-
+        
         self.button1 = wx.Button(self.panel, label = "Convert", pos = (150,450), size= (70,30))
-        self.button2 = wx.Button(self.panel, label = "Close", pos = (300,450), size=(70,30))
-        self.tickets = wx.TextCtrl(self.panel, pos = (120,150), size=(300,250))
+        self.button2 = wx.Button(self.panel, label = "Clear", pos = (300,450), size=(70,30))
+        
+        self.tickets = wx.TextCtrl(self.panel, pos = (120,150), size=(300,250),
+            style = wx.TE_MULTILINE|wx.TE_NO_VSCROLL|wx.BORDER_NONE)
         
 
-        self.result = wx.StaticText(self.panel, pos = (500,200))
+        self.result = wx.StaticText(self.panel, pos = (500,200),
+            style = wx.TE_READONLY|wx.TE_MULTILINE|wx.BORDER_NONE|wx.TE_NO_VSCROLL)
+        #self.result.Wrap(30)
         font2 = self.result.GetFont()
         font2.PointSize = 10
         self.result.SetFont(font2)
@@ -29,16 +33,25 @@ class TicketCountFrame(wx.Frame):
 
 
         self.button1.Bind(wx.EVT_BUTTON, self.Convert)
-        self.button2.Bind(wx.EVT_BUTTON, self.ExitApp)
+        self.button2.Bind(wx.EVT_BUTTON, self.ClearText)
+
+   
+
 
 
     def Convert(self, event):
-        self.result.SetLabel(self.tickets.GetValue())
+        url = self.tickets.GetValue()
+        id1 = re.findall("\d+",url)
+        self.result.SetLabel(','.join(id1))
+
         
 
-    def ExitApp(self, event):
+    def ClearText(self, event):
+        self.result.SetLabel("")
+        self.tickets.SetValue("")
 
-        self.Close(True)
+    
+        
 
 app = wx.App(False)
 frame = TicketCountFrame()
