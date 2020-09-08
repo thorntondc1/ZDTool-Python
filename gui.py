9a98a4e17af9c1a4028cc1f2c8007d8e0d2e441c
thorntondc1 <thorntondc1@gmail.com>
2020-09-08 12:41:25 -0500

    Changed the way gui displays text. Added an export to txt option

--- gui.py
+++ gui.py
@@ -1,54 +1,103 @@
 import wx
 import re
+from datetime import date
 
 class TicketCountFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, title="ZDTool", pos=(100,150), size=(800,600))
+        wx.Frame.__init__(self, None, title="ZDTool", pos=(100,150), size=(700,500))
 
+        #setting up panel and static text
         self.panel = wx.Panel(self)
-
-        self.text = wx.StaticText(self.panel, label = "Enter Tickets:", pos = (10,150))
+        self.text = wx.StaticText(self.panel, label = "Enter Tickets:", pos = (10,50))
         font = self.text.GetFont()
         font.PointSize = 12
         font = font.Bold()
         self.text.SetFont(font)
         
-        self.button1 = wx.Button(self.panel, label = "Convert", pos = (150,450), size= (70,30))
-        self.button2 = wx.Button(self.panel, label = "Clear", pos = (300,450), size=(70,30))
+        #self.logo = wx.StaticBitmap(self, -1, wx.Bitmap("C:\ZDTOOL\logo.jpg",wx.BITMAP_TYPE_ANY))
+
+        #setting up buttons
+        self.button1 = wx.Button(self.panel, label = "Convert", pos = (150,350), size= (70,30))
+        self.button2 = wx.Button(self.panel, label = "Clear", pos = (300,350), size=(70,30))
         
-        self.tickets = wx.TextCtrl(self.panel, pos = (120,150), size=(300,250),
+        #user text setup
+        self.tickets = wx.TextCtrl(self.panel, pos = (120,50), size=(300,250),
             style = wx.TE_MULTILINE|wx.TE_NO_VSCROLL|wx.BORDER_NONE)
         
-
-        self.result = wx.StaticText(self.panel, pos = (500,200),
-            style = wx.TE_READONLY|wx.TE_MULTILINE|wx.BORDER_NONE|wx.TE_NO_VSCROLL)
-        #self.result.Wrap(30)
-        font2 = self.result.GetFont()
-        font2.PointSize = 10
-        self.result.SetFont(font2)
         
 
-
-
-
-
+        #bind tasks to buttons 
         self.button1.Bind(wx.EVT_BUTTON, self.Convert)
         self.button2.Bind(wx.EVT_BUTTON, self.ClearText)
 
+        #create a menu and status bar 
+        self.MenuBar()
+
+        self.CreateStatusBar()
+        self.SetStatusText("Use this tool to strip ticket numbers from URLs!")
+
    
+        #Giving menu bar items 
+    def MenuBar(self):
+        fileMenu = wx.Menu()
+ 
+        exportItem = fileMenu.Append(-1, "&Export\tCtrl-E",
+                "Export text file with IDs")
+        fileMenu.AppendSeparator()
+        closeItem = fileMenu.Append(wx.ID_EXIT)
+
+        # Now a help menu for the about item
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+ 
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        self.SetMenuBar(menuBar)
 
 
+        #Giving buttons functionality 
+        self.Bind(wx.EVT_MENU, self.CloseApp, closeItem)
+        self.Bind(wx.EVT_MENU, self.About, aboutItem)
+        self.Bind(wx.EVT_MENU, self.Export, exportItem)
 
+        
+        #Closes app
+    def CloseApp(self,event):
+        self.Close(True)    
+
+
+        #Pulls ticket numbers from URLs, if there is no URL and jsut a number, it should pull that and place it into a string that is a list. 
     def Convert(self, event):
         url = self.tickets.GetValue()
         id1 = re.findall("\d+",url)
-        self.result.SetLabel(','.join(id1))
+        self.tickets.SetValue(','.join(id1))
 
-        
 
+            
+        #clears user input or output for more URLs. 
     def ClearText(self, event):
-        self.result.SetLabel("")
         self.tickets.SetValue("")
+
+    def About(self,event):
+        wx.MessageBox("Use this to pull ticket numbers from ticket URLs. Export function should create a text file.",
+                        "About ZDTOOL - Python",
+                        wx.OK|wx.ICON_INFORMATION)
+
+
+    def Export(self,event):
+        if self.tickets.GetValue() != "":
+            file = open("ZDTOOL Exports.txt","a")
+            today = date.today()
+            file.write(today.strftime("%b-%d-%Y:  \n "))
+            file.write("{} \n".format(self.tickets.GetValue()))
+            file.write('\n')
+            file.close()
+        else:
+            wx.MessageBox("Ticket field is empty. Please paste and convert tickets to export.","ZDTOOL Export",
+                            wx.OK|wx.ICON_INFORMATION)
 
     
         
